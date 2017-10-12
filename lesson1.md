dc831021d47c81c0d5b119f049c9cab64cb61743
yokoyan <yokoyansan@gmail.com>
2017-10-12 04:05:47 +0000

    #06まで学習

---
+++ lesson1.md
@@ -0,0 +1,19 @@
+# RSpec
+
+## TDD => BDD
+- テストフレームワーク
+- Test Driven Development
+- 振る舞い駆動開発（Behavior Driven Development）
+
+# 公式サイト
+www.relishapp.com
+
+## 知識
+- Ruby
+- Unixコマンド
+
+## 環境
+- ローカル開発環境（Vagrant）
+- ruby -v 2.3.0
+- rspec -v 3.4.4
+
---
+++ spec/cals_spec.rb
@@ -0,0 +1,24 @@
+require 'calc'
+
+RSpec.describe "A calc" do
+  # 共通化のためのbeforeブロック
+  # :exampleで、それぞれのexample開始前に実行する
+  # :contextで、最初の1回だけ実行する
+  # 省略すると、:exampleになる
+  before do
+    # テストコード全体で参照する変数は、@をつける
+    @calc = Calc.new
+  end
+
+  # itで囲まれた範囲がexample
+  # 期待される振る舞いのこと
+  it "given 2 and 3, returns 5" do
+    # 期待値:expectで記述する
+    expect(@calc.add(2, 3)).to eq(5)
+  end
+  # 三角測量（違った角度からテストを加えること）
+  it "given 5 and 8, returns 13" do
+    # 期待値:expectで記述する
+    expect(@calc.add(5, 8)).to eq(13)
+  end
+end
